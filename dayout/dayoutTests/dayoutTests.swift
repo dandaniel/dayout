6eb20119713cbd3fcb73d379af26f9cbad448830
Dan Mindru <danmindru@Dans-MacBook-Pro.local>
2014-06-14 13:46:55 +0200

    initialized project

---
+++ dayout/dayoutTests/dayoutTests.swift
@@ -0,0 +1,35 @@
+//
+//  dayoutTests.swift
+//  dayoutTests
+//
+//  Created by Dan Mindru on 14/06/14.
+//  Copyright (c) 2014 Dan Mindru. All rights reserved.
+//
+
+import XCTest
+
+class dayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
